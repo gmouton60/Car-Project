cbf35e2db4ffa1d5afeba1b236c617ccd4751e26
gmouton60 <37676928+gmouton60@users.noreply.github.com>
2019-11-09 09:33:49 -0600

    Update CarProject.java

--- CarProject.java
+++ CarProject.java
@@ -1,13 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-//Student Name: Gerald Mouton
-//LSU ID: 894569516
-//Lab Section:1
-//Assignment Lab 2
-//Submission Time: 5:25
 package carproject;
 import java.io.File;
 import java.io.PrintWriter;
